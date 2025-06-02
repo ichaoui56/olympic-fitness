0549837cd61ac3c37e80aee0986afa5c6ea7f848
CnomDev2 <dev2@cnomweb.com>
2025-06-02 16:21:23 +0100

    add the title for tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,33 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Dumbbell } from "lucide-react"
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Olympic Fitness",
   description: "Olympic Fitness - The Best Fitness Center in Salé",
-}
+  icons: {
+    icon: "/logo-cnom.png",
+    apple: "/logo-cnom.png",
+    shortcut: "/logo-cnom.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${inter.className} bg-dark-bg-primary text-dark-text-primary`}>{children}</body>
+      <body
+        className={`${inter.className} bg-dark-bg-primary text-dark-text-primary`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
